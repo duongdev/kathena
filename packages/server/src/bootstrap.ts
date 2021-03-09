43f3658ddb7e2e92a17afb23f90715556c72e0e7
Dustin Do <dustin.do95@gmail.com>
2021-03-09 20:18:20 +0700

    [server] Migrate from fastify to express
    CU-#6xycj2[completed]

--- packages/server/src/bootstrap.ts
+++ packages/server/src/bootstrap.ts
@@ -1,8 +1,4 @@
 import { NestFactory } from '@nestjs/core'
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify'
 import { Logger } from 'core/Logger'
 import { AppModule } from 'core/app'
 import { config } from 'core'
@@ -10,10 +6,7 @@ import { config } from 'core'
 const logger = new Logger('bootstrap')
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  )
+  const app = await NestFactory.create(AppModule)
   const port = config.PORT
 
   await app.listen(port, '0.0.0.0', () => {
