625fe2491fee4ca9eaff236bbb8e99b0602ce021
Dustin Do <Dustin.Do@nab.com.au>
2021-03-12 11:45:11 +0700

    Update theme color

--- packages/web/src/@kathena/theme/theme.constants.ts
+++ packages/web/src/@kathena/theme/theme.constants.ts
@@ -1,7 +1,6 @@
-export const BORDER_RADIUS = 4
-export const PRIMARY_COLOR = '#103955'
-export const SECONDARY_COLOR = '#FFC82D'
-export const ROUNDED_CORNER_RADIUS = 64
+export const BORDER_RADIUS = 8
+export const PRIMARY_COLOR = '#2e69ff'
+export const SECONDARY_COLOR = '#fe445b'
 
 export const TEXT_FONT = `"OpenSans","Helvetica","Arial",sans-serif`
 export const HEADING_FONT = `"Proxima Nova","OpenSans","Helvetica","Arial",sans-serif`
