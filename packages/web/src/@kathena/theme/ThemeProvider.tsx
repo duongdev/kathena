9c2f9d68f46fda397ecd5bc2835d19ac829720a2
Dustin Do <dustin.do95@gmail.com>
2021-03-13 21:18:44 +0700

    [ThemeProvider] Add global styles

--- packages/web/src/@kathena/theme/ThemeProvider.tsx
+++ packages/web/src/@kathena/theme/ThemeProvider.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 
 import {
   CssBaseline,
+  makeStyles,
   ThemeProvider as MuiThemeProvider,
 } from '@material-ui/core'
 import StyledEngineProvider from '@material-ui/core/StyledEngineProvider'
@@ -17,8 +18,25 @@ export const ThemeProvider: FC<ThemeProviderProps> = (props) => {
     <StyledEngineProvider injectFirst>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
+        <GlobalStyles />
         {children}
       </MuiThemeProvider>
     </StyledEngineProvider>
   )
 }
+
+const GlobalStyles = () => {
+  useGlobalStyles()
+  return null
+}
+
+const useGlobalStyles = makeStyles(({ transitions }) => ({
+  '@global': {
+    body: {
+      transition: transitions.create('background-color', {
+        duration: transitions.duration.standard,
+        easing: transitions.easing.easeInOut,
+      }),
+    },
+  },
+}))
