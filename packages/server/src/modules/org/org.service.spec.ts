533e3a07c5ccdc8b3e83c512b37e515995b9347f
Nguyễn Văn Hải <61925973+nguyenvanhai-0911@users.noreply.github.com>
2021-03-24 22:39:01 +0700

    Update packages/server/src/modules/org/org.service.spec.ts
    Co-authored-by: Dustin Do <dustin.do95@gmail.com>

--- packages/server/src/modules/org/org.service.spec.ts
+++ packages/server/src/modules/org/org.service.spec.ts
@@ -63,7 +63,7 @@ describe('org.service', () => {
   })
 
   describe('findOrgByNamespace', () => {
-    it(`return null if OrgByNamespace is empty`, async () => {
+    it(`returns null if org namespace is empty`, async () => {
       expect.assertions(1)
 
       await expect(orgService.findOrgByNamespace('')).resolves.toBeNull()
