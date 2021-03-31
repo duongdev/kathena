eaaa5fc77c10d2ec29ec6cf1800e78aee9d970ac
HuynhThanhCanh <huynhthanhcanh.top@gmail.com>
2021-03-31 12:59:45 +0700

    Setup validate for academic test

---
+++ packages/server/src/modules/academic/academic.service.spec.ts
@@ -0,0 +1,37 @@
+import { TestingModule } from '@nestjs/testing'
+import { Connection } from 'mongoose'
+
+import { objectId } from 'core/utils/db'
+import { createTestingModule, initTestDb } from 'core/utils/testing'
+import { ANY } from 'types'
+
+import { AcademicService } from './academic.service'
+
+describe('academic.service', () => {
+  let module: TestingModule
+  let academicService: AcademicService
+  let mongooseConnection: Connection
+
+  beforeAll(async () => {
+    const testDb = await initTestDb()
+    mongooseConnection = testDb.mongooseConnection
+
+    module = await createTestingModule(testDb.uri)
+
+    academicService = module.get<AcademicService>(AcademicService)
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  beforeEach(async () => {
+    await mongooseConnection.dropDatabase()
+    jest.resetAllMocks()
+    jest.restoreAllMocks()
+  })
+
+  it('should be defined', () => {
+    expect(academicService).toBeDefined()
+  })
+})
