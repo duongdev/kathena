6ff6c7e9f8711fa1f564c3ce8c062c6e230e267f
nhatminh0509 <leminhnhat1133@gmail.com>
2021-04-01 15:12:35 +0700

    Fix error

--- packages/server/src/modules/academic/academicSubject.resolver.ts
+++ packages/server/src/modules/academic/academicSubject.resolver.ts
@@ -35,6 +35,7 @@ export class AcademicSubjectResolver {
 
     const { createReadStream, filename, encoding } = image
 
+    // eslint-disable-next-line no-console
     console.log('encoding', encoding)
 
     const imageFile = await this.fileStorageService.uploadFromReadStream({
