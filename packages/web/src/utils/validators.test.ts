53ebae5e33ee6735d38de31fd7a25df239c90bfc
@buihuyhoang9a3 <buihuyhoang9a3@gmail.com>
2021-04-04 11:01:35 +0700

    CU-7hvnxv - Fix validator ORG_OFFICE_NAME_REGEX & ACADEMIC_SUBJECT_NAME_REGEX

--- packages/web/src/utils/validators.test.ts
+++ packages/web/src/utils/validators.test.ts
@@ -2,14 +2,37 @@ import {
   DISPLAY_NAME_REGEX,
   USERNAME_REGEX,
   ORG_OFFICE_NAME_REGEX,
+  ACADEMIC_SUBJECT_NAME_REGEX,
 } from './validators'
 
-test('ORG_OFFICE_NAME_REGEX', () => {
-  expect(ORG_OFFICE_NAME_REGEX.test('Bùi Huy Hoàng')).toBe(true)
-  expect(ORG_OFFICE_NAME_REGEX.test('Bùi Huy Hoàng1')).toBe(true)
-  expect(ORG_OFFICE_NAME_REGEX.test('1Bùi Huy Hoàng')).toBe(true)
+test('ACADEMIC_SUBJECT_NAME_REGEX', () => {
+  expect(
+    ACADEMIC_SUBJECT_NAME_REGEX.test('Lập trình web Backend với PHP cơ bản'),
+  ).toBe(true)
+  expect(ACADEMIC_SUBJECT_NAME_REGEX.test('Lập trình web')).toBe(true)
 
-  expect(ORG_OFFICE_NAME_REGEX.test('Bùi Huy Hoàng@')).toBe(false)
+  expect(
+    ACADEMIC_SUBJECT_NAME_REGEX.test(
+      'Lập trình web Back-end với PHP cơ bản - Khóa T4',
+    ),
+  ).toBe(false)
+  expect(
+    ACADEMIC_SUBJECT_NAME_REGEX.test('Lập trình web Back-end với PHP cơ bản'),
+  ).toBe(false)
+  expect(
+    ACADEMIC_SUBJECT_NAME_REGEX.test('Lập trình web Backend với PHP cơ bản1'),
+  ).toBe(false)
+  expect(
+    ACADEMIC_SUBJECT_NAME_REGEX.test('Lập trình web Backend với PHP cơ bản@'),
+  ).toBe(false)
+})
+
+test('ORG_OFFICE_NAME_REGEX', () => {
+  expect(ORG_OFFICE_NAME_REGEX.test('Kmin Quận 1')).toBe(true)
+  expect(ORG_OFFICE_NAME_REGEX.test('Kmin')).toBe(true)
+  expect(ORG_OFFICE_NAME_REGEX.test('Quận 1 Kmin')).toBe(true)
+
+  expect(ORG_OFFICE_NAME_REGEX.test('Kmin@')).toBe(false)
 })
 
 test('DISPLAY_NAME_REGEX', () => {
