5f0f6d60ce11ecb49ee4846eb0fe3fc0f1864424
Cảnh Huỳnh <61925916+HuynhThanhCanh@users.noreply.github.com>
2021-04-04 20:53:46 +0700

    Add unit test for academicService.findAcademicSubjectByCode (#141)
    * CU-7dwtqz - Check and write unit test for academicService.findAcademicSubjectByCode
    
    * Delete unused imports
    
    * Update packages/server/src/modules/academic/academic.service.spec.ts
    
    Co-authored-by: Liêm Đặng <60537515+danghieuliem@users.noreply.github.com>
    
    Co-authored-by: Liêm Đặng <60537515+danghieuliem@users.noreply.github.com>

--- packages/server/src/modules/academic/academic.service.spec.ts
+++ packages/server/src/modules/academic/academic.service.spec.ts
@@ -241,4 +241,53 @@ describe('academic.service', () => {
       })
     })
   })
+
+  describe('findAcademicSubjectByCode', () => {
+    it(`returns null if code or orgId doesn't exist`, async () => {
+      expect.assertions(2)
+
+      await expect(
+        academicService.findAcademicSubjectByCode('NESTJS-T1-2021', objectId()),
+      ).resolves.toBeNull()
+
+      await expect(
+        academicService.findAcademicSubjectByCode('NESTJS-T1-2021'),
+      ).resolves.toBeNull()
+    })
+
+    it('returns an academic subject if code exists', async () => {
+      expect.assertions(2)
+
+      const academicSubject: ANY = {
+        code: 'NESTJS-T1-2021',
+        name: 'NestJs',
+        description: 'This is NestJs course',
+        publication: 'Draft',
+        createdByAccountId: objectId(),
+      }
+
+      jest
+        .spyOn(academicService['academicSubjectModel'], 'findOne')
+        .mockResolvedValueOnce(academicSubject)
+        .mockResolvedValueOnce(academicSubject)
+
+      await expect(
+        academicService.findAcademicSubjectByCode('NESTJS-T1-2021'),
+      ).resolves.toMatchObject({
+        code: 'NESTJS-T1-2021',
+        name: 'NestJs',
+        description: 'This is NestJs course',
+        publication: 'Draft',
+      })
+
+      await expect(
+        academicService.findAcademicSubjectByCode('NESTJS-T1-2021', objectId()),
+      ).resolves.toMatchObject({
+        code: 'NESTJS-T1-2021',
+        name: 'NestJs',
+        description: 'This is NestJs course',
+        publication: 'Draft',
+      })
+    })
+  })
 })
