d438ddc628a0e4bd9611ca0a89b0cb27c82107c1
Cảnh Huỳnh <61925916+HuynhThanhCanh@users.noreply.github.com>
2021-05-01 00:51:33 +0700

    Implement academicService.addLecturesToCourse (#234)

--- packages/server/src/modules/auth/models/Permission.ts
+++ packages/server/src/modules/auth/models/Permission.ts
@@ -22,6 +22,7 @@ export enum Permission {
   Academic_CreateCourse = 'Academic_CreateCourse',
   Academic_UpdateCourse = 'Academic_UpdateCourse',
   Academic_ListCourses = 'Academic_ListCourses',
+  Academic_AddLecturersToCourse = 'Academic_AddLecturersToCourse',
   Academic_RemoveStudentsFromCourse = 'Academic_RemoveStudentsFromCourse',
   Academic_RemoveLecturersFromCourse = 'Academic_RemoveLecturersFromCourse',
 
