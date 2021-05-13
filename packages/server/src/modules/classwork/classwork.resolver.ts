d874ca7e7a79e2312365f279b1088a044118bfee
Cảnh Huỳnh <61925916+HuynhThanhCanh@users.noreply.github.com>
2021-05-13 13:08:18 +0700

    Update classwork models (#291)
    * CU-8e1c6e - [BE] Update classwork models
    
    * Fix missing return type on function @typescript-eslint/explicit-function-return-type
    
    * Update classwork.type.ts

---
+++ packages/server/src/modules/classwork/classwork.resolver.ts
@@ -0,0 +1,45 @@
+import {
+  ResolveField,
+  /** Args,
+  ID,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField, */
+  Resolver,
+} from '@nestjs/graphql'
+
+import { ANY } from 'types'
+// import { differenceInMinutes } from 'date-fns'
+// import { ForbiddenError } from 'type-graphql'
+
+// import { CurrentAccount, CurrentOrg, UseAuthGuard } from 'core/auth'
+// import { P } from 'modules/auth/models'
+// import { Org } from 'modules/org/models/Org'
+// import { Nullable, PageOptionsInput } from 'types'
+
+import { ClassworkType } from './models/Classwork'
+
+@Resolver('ResultClassworkUnion')
+export class ResultClassworkUnionResolver {
+  /**
+   *START RESULT CLASSWORK UNION RESOLVER
+   */
+
+  @ResolveField()
+  resolveType(value: { type: ClassworkType }): [...ANY] {
+    if (value.type === ClassworkType.Material) {
+      return ['ClassworkMaterial']
+    }
+
+    if (value.type === ClassworkType.Assignment) {
+      return ['ClassworkAssignment']
+    }
+
+    return ['ClassworkMaterial', 'ClassworkAssignment']
+  }
+
+  /**
+   * END RESULT CLASSWORK UNION RESOLVER
+   */
+}
