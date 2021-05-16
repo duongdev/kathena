5a47995188cb4eaac99cbbf205991576a9a7f36d
nguyenvanhai <vanhai14100911@gmail.com>
2021-05-16 11:10:07 +0700

    Implement classworkService.createClassworkAssignments

--- packages/server/src/modules/classwork/classwork.service.ts
+++ packages/server/src/modules/classwork/classwork.service.ts
@@ -1,10 +1,10 @@
 import { forwardRef, Inject } from '@nestjs/common'
-import { /** DocumentType */ ReturnModelType } from '@typegoose/typegoose'
+import { DocumentType, ReturnModelType } from '@typegoose/typegoose'
 // import * as bcrypt from 'bcrypt'
 // import { uniq } from 'lodash'
 // import { ForbiddenError } from 'type-graphql'
 
-import { Service, InjectModel, Logger } from 'core'
+import { Service, InjectModel, Logger, Publication } from 'core'
 // import { isObjectId } from 'core/utils/db'
 // import {
 //   removeExtraSpaces,
@@ -13,6 +13,8 @@ import { Service, InjectModel, Logger } from 'core'
 import { AuthService } from 'modules/auth/auth.service'
 // import { OrgRoleName, Permission } from 'modules/auth/models'
 import { OrgService } from 'modules/org/org.service'
+
+import { CreateClassworkAssignmentInput } from './classwork.type'
 // import { ANY, Nullable } from 'types'
 
 import { ClassworkAssignment } from './models/ClassworkAssignment'
@@ -54,7 +56,54 @@ export class ClassworkService {
    * START CLASSWORK ASSIGNMENT
    */
 
-  // TODO: Delete this line and start the code here
+  async createClassworkAssignment(
+    createdByAccountId: string,
+    courseId: string,
+    orgId: string,
+    classworkAssignmentInput: CreateClassworkAssignmentInput,
+  ): Promise<DocumentType<ClassworkAssignment>> {
+    const {
+      title,
+      description,
+      attachments,
+      dueDate,
+    } = classworkAssignmentInput
+
+    if (!(await this.orgService.validateOrgId(orgId))) {
+      throw new Error(`Org ID is invalid`)
+    }
+
+    // Can create ClassworkAssignments
+    const canCreateClassworkAssignment = await this.authService.canAccountManageCourse(
+      createdByAccountId,
+      courseId,
+    )
+
+    if (!canCreateClassworkAssignment) {
+      throw new Error(`CAN_NOT_CREATE_CLASSWORK_ASSIGNMENT`)
+    }
+
+    const currentDate = new Date()
+    const startDateInput = new Date(dueDate)
+
+    if (
+      startDateInput.setHours(7, 0, 0, 0) < currentDate.setHours(7, 0, 0, 0)
+    ) {
+      throw new Error(`START_DATE_INVALID`)
+    }
+
+    const classworkAssignment = this.classworkAssignmentsModel.create({
+      createdByAccountId,
+      courseId,
+      title,
+      description,
+      attachments,
+      publicationState: Publication.Draft,
+      dueDate,
+    })
+
+    return classworkAssignment
+  }
 
   /**
    * END CLASSWORK ASSIGNMENT
