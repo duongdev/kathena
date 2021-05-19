633808a284a91daaabd162d8b4a76c2fcb201f4e
kugayamashusei <58841667+kugayamashusei@users.noreply.github.com>
2021-05-19 17:04:03 +0700

    Cu 8fvxjz update UI add remove teacher student to from course page (#298)
    * crate router
    
    * appear the lecturer
    
    * update times 2
    
    * done background lecturer
    
    * create PopoverLecturer
    
    * update fomik lecturer
    
    * update fomik times 2
    
    * get lecturerIds, but don't get courseId
    
    * fix bug
    
    * updata id findLecturerCourse
    
    * add lecturer and student to course
    
    * fix bug and you have add lecturer/student
    
    * update background
    
    * delete lecturer
    
    * CU-83u2t7 - [FE] As a staff, I want to add/remove lectures/students to/from a Course
    
    * fix bug render when admin delete lecturer/student
    
    * fix name
    
    * fix width select lecturer/student
    
    * CU-8fvxjz - Update UI Add/Remove Teacher/Student To/From Course Page
    
    * update
    
    Co-authored-by: Dustin Do <dustin.do95@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/web/src/modules/UpdateCourses/AccountUserName.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { FC, useMemo } from 'react'
+
+import { makeStyles, Skeleton } from '@material-ui/core'
+import clsx from 'clsx'
+
+import { Typography, TypographyProps } from '@kathena/ui'
+import { Account, useAccountDisplayNameQuery } from 'graphql/generated'
+import useAccountUtils from 'utils/useAccountUtils'
+
+export type AccountUserNameWithId = {
+  accountId: string
+}
+
+export type AccountUserNameWithAccount = {
+  account: Pick<Account, 'id' | 'username' | 'displayName'>
+}
+
+export type AccountUserNameProps = TypographyProps &
+  (AccountUserNameWithId | AccountUserNameWithAccount)
+
+const AccountUserName: FC<AccountUserNameProps> = (props) => {
+  const { className, ...TypoProps } = props
+  const { accountId } = props as AccountUserNameWithId
+  const { account: accountProp } = props as AccountUserNameWithAccount
+
+  const classes = useStyles(props)
+  const { getUserName } = useAccountUtils()
+  const { data, loading } = useAccountDisplayNameQuery({
+    variables: { id: accountId },
+    skip: !!accountProp,
+  })
+
+  const account = useMemo(() => accountProp || data?.account, [
+    accountProp,
+    data?.account,
+  ])
+  const username = useMemo(() => (account ? getUserName(account) : ''), [
+    account,
+    getUserName,
+  ])
+
+  if (loading) return <Skeleton variant="text" className={classes.skeleton} />
+
+  return (
+    <Typography
+      className={clsx(className, classes.root)}
+      title={username}
+      {...TypoProps}
+    >
+      @{username}
+    </Typography>
+  )
+}
+
+const useStyles = makeStyles(() => ({
+  root: {
+    fontWeight: 'bold',
+    cursor: 'default',
+    maxWidth: 250,
+    overflow: 'hidden',
+  },
+  skeleton: {
+    maxWidth: 180,
+    height: 27,
+  },
+}))
+
+export default AccountUserName
