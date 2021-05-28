0b2f6d6d4119348078182bba0cbd84cc5e903e1c
@buihuyhoang9a3 <buihuyhoang9a3@gmail.com>
2021-05-28 15:02:51 +0700

    update User Interface DetailClassworkMaterials

--- packages/web/src/modules/TeachingCourse/Components/ClassworkMaterials/DetailClassworkMaterials.tsx
+++ packages/web/src/modules/TeachingCourse/Components/ClassworkMaterials/DetailClassworkMaterials.tsx
@@ -2,25 +2,44 @@ import { FC, useMemo } from 'react'
 
 import { CardContent, Grid, makeStyles } from '@material-ui/core'
 import format from 'date-fns/format'
-import { useSnackbar } from 'notistack'
 import { useParams } from 'react-router-dom'
 
 import { DASHBOARD_SPACING } from '@kathena/theme'
-import { InfoBlock, PageContainer, SectionCard, Typography } from '@kathena/ui'
+import { ANY } from '@kathena/types'
+import {
+  InfoBlock,
+  PageContainer,
+  SectionCard,
+  SectionCardSkeleton,
+  Typography,
+} from '@kathena/ui'
+import { useAuth } from 'common/auth'
 import { useDetailClassworkMaterialQuery } from 'graphql/generated'
 
 export type DetailClassworkMaterialsProps = {}
 
 const DetailClassworkMaterials: FC<DetailClassworkMaterialsProps> = (props) => {
   const classes = useStyles(props)
-  const { enqueueSnackbar } = useSnackbar()
   const params: { id: string } = useParams()
   const id = useMemo(() => params.id, [params.id])
+  const { account } = useAuth()
 
-  const { data } = useDetailClassworkMaterialQuery({
+  const { data, loading } = useDetailClassworkMaterialQuery({
     variables: { Id: id },
   })
   const classworkMaterial = useMemo(() => data?.classworkMaterial, [data])
+  if (loading) {
+    return (
+      <Grid container spacing={DASHBOARD_SPACING}>
+        <Grid item xs={9}>
+          <SectionCardSkeleton />
+        </Grid>
+        <Grid item xs={3}>
+          <SectionCardSkeleton />
+        </Grid>
+      </Grid>
+    )
+  }
   return (
     <PageContainer
       withBackButton
@@ -34,23 +53,35 @@ const DetailClassworkMaterials: FC<DetailClassworkMaterialsProps> = (props) => {
           title="Thông tin tài liệu"
         >
           <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                Giảng viên thêm: {`${classworkMaterial?.createdByAccountId}`}
-              </Grid>
-              <InfoBlock gridItem={{ xs: 9 }} label="Tiêu đề:">
+            <Grid container spacing={2} className={classes.root}>
+              <InfoBlock gridItem={{ xs: 10 }} label="  Giảng viên thêm:">
+                <Typography variant="h5">{`${account?.displayName}`}</Typography>
+              </InfoBlock>
+              <InfoBlock gridItem={{ xs: 2 }} label="Ngày đăng:">
+                {format(new Date(classworkMaterial?.createdAt), 'dd/MM/yyyy')}
+              </InfoBlock>
+              <InfoBlock gridItem={{ xs: 12 }} label="Tiêu đề:">
                 {`${classworkMaterial?.title}`}
               </InfoBlock>
-              <InfoBlock gridItem={{ xs: 3 }} label="Ngày đăng:">
-                <Typography>
-                  {format(new Date(classworkMaterial?.createdAt), 'dd/MM/yyyy')}
-                </Typography>
-              </InfoBlock>
               <InfoBlock gridItem={{ xs: 12 }} label="Mô tả:">
-                {`${classworkMaterial?.description}`}
+                <div
+                  // eslint-disable-next-line
+                  dangerouslySetInnerHTML={{
+                    __html: classworkMaterial?.description as ANY,
+                  }}
+                  style={{
+                    display: '-webkit-box',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    WebkitLineClamp: 1,
+                    WebkitBoxOrient: 'vertical',
+                  }}
+                />
               </InfoBlock>
-              <InfoBlock gridItem={{ xs: 12 }} label="Tài liệu">
-                {`${classworkMaterial?.attachments}`}
+              <InfoBlock gridItem={{ xs: 12 }} label="Tài liệu:">
+                {classworkMaterial?.attachments.map((attachment) => (
+                  <Grid container>{attachment}</Grid>
+                ))}
               </InfoBlock>
             </Grid>
           </CardContent>
