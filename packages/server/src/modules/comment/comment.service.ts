3f44329b138da23a230a4ec39658fbd72896d51f
Cảnh Huỳnh <61925916+HuynhThanhCanh@users.noreply.github.com>
2021-06-01 23:30:55 +0700

    Implement commentService.listCommentByTargetId (#389)
    * CU-8u0euq - [BE] Implement commentService.listCommentByTargetId
    
    * Edit ResolveField comment
    
    * Delete TODO
    
    * Write unit test
    
    * Fix error lint

--- packages/server/src/modules/comment/comment.service.ts
+++ packages/server/src/modules/comment/comment.service.ts
@@ -4,7 +4,7 @@ import { DocumentType, ReturnModelType } from '@typegoose/typegoose'
 import { Service, InjectModel, Logger } from 'core'
 import { OrgService } from 'modules/org/org.service'
 
-import { CreateCommentInput } from './comment.type'
+import { CommentPageOptionInput, CreateCommentInput } from './comment.type'
 import { Comment } from './model/Comment'
 
 @Service()
@@ -39,5 +39,40 @@ export class CommentService {
     return comment
   }
 
-  // TODO: Implement commentService.listCommentByTargetId
+  async listCommentByTargetId(
+    commentPageOptionInput: CommentPageOptionInput,
+    filter: {
+      orgId: string
+      lastId?: string
+      targetId: string
+    },
+  ): Promise<{ comments: DocumentType<Comment>[]; count: number }> {
+    const { limit } = commentPageOptionInput
+    const { orgId, lastId, targetId } = filter
+
+    if (!(await this.orgService.validateOrgId(orgId)))
+      throw new Error('ORG_ID_INVALID')
+
+    const getComments = this.commentModel.find({
+      targetId,
+    })
+
+    if (lastId) {
+      getComments.find({
+        $and: [
+          {
+            _id: {
+              $lt: lastId,
+            },
+          },
+        ],
+      })
+    }
+
+    getComments.sort({ _id: -1 }).limit(limit)
+    const comments = await getComments
+    const count = await this.commentModel.countDocuments({ orgId, targetId })
+
+    return { comments, count }
+  }
 }
--- packages/server/src/modules/comment/comment.type.ts
+++ packages/server/src/modules/comment/comment.type.ts
@@ -1,5 +1,22 @@
-import { Field, ID, InputType } from '@nestjs/graphql'
+import { Field, InputType, ID, Int, ObjectType } from '@nestjs/graphql'
 import { IsNotEmpty } from 'class-validator'
+
+import { Comment } from 'modules/comment/model/Comment'
+
+@InputType()
+export class CommentPageOptionInput {
+  @Field((_type) => Int)
+  limit: number
+}
+
+@ObjectType()
+export class CommentsPayload {
+  @Field((_type) => [Comment])
+  comments: Comment[]
+
+  @Field((_type) => Int)
+  count: number
+}
 
 @InputType()
 export class CreateCommentInput {
