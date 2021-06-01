7346c97ec6dbfe3ddb05e80d38c8338da7377beb
Liêm Đặng <60537515+danghieuliem@users.noreply.github.com>
2021-06-01 19:12:36 +0700

    Add and write test classwork servicecreate classwork submission (#383)
    * CU-8qzveh - [BE] Update classwork.service.createClassworkMaterial (add file)
    
    * update args move attachmentsInput to createClassworkMaterialInput
    
    * fix publicationState
    
    * fix
    
    * CU-8u0d5j - [BE] Implement classworkService.createClassworkSubmission
    
    * fix
    
    * fix
    
    * fix
    
    Co-authored-by: Nhật Lê <73304548+nhatminh0509@users.noreply.github.com>
    Co-authored-by: Nguyễn Văn Hải <61925973+nguyenvanhai-0911@users.noreply.github.com>

--- packages/server/src/modules/auth/models/Permission.ts
+++ packages/server/src/modules/auth/models/Permission.ts
@@ -47,6 +47,7 @@ export enum Permission {
   Classwork_AddAttachmentsToClassworkMaterial = 'Classwork_AddAttachmentsToClassworkMaterial',
   Classwork_RemoveAttachmentsFromClassworkMaterial = 'Classwork_RemoveAttachmentsFromClassworkMaterial',
 
+  Classwork_CreateClassworkSubmission = 'Classwork_CreateClassworkSubmission',
   /** For testing purpose */
   NoPermission = 'NoPermission',
 }
