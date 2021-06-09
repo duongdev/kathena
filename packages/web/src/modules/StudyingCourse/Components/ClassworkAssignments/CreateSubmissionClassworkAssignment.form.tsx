dd0fa89b8ca24386f74d1890e03dbd1b3fdfafae
@buihuyhoang9a3 <buihuyhoang9a3@gmail.com>
2021-06-09 15:25:12 +0700

    Update UI time2

--- packages/web/src/modules/StudyingCourse/Components/ClassworkAssignments/CreateSubmissionClassworkAssignment.form.tsx
+++ packages/web/src/modules/StudyingCourse/Components/ClassworkAssignments/CreateSubmissionClassworkAssignment.form.tsx
@@ -1,6 +1,6 @@
 import { FC, useCallback } from 'react'
 
-import { CardContent, Grid, makeStyles, Stack } from '@material-ui/core'
+import { CardContent, Grid, makeStyles } from '@material-ui/core'
 import { useFormikContext } from 'formik'
 
 import { DASHBOARD_SPACING } from '@kathena/theme'
@@ -40,13 +40,11 @@ const CreateSubmissionClassworkAssignmentForm: FC<CreateSubmissionClassworkAssig
           title="Thông tin bài tập"
         >
           <CardContent>
-            <Stack mt={3}>
-              <EditorFormField
-                required
-                name="description"
-                label={labels.description}
-              />
-            </Stack>
+            <EditorFormField
+              required
+              name="description"
+              label={labels.description}
+            />
           </CardContent>
         </SectionCard>
         <SectionCard
