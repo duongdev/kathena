445b7a0d50797c54d21e449ad1cb9007658d5dfc
@buihuyhoang9a3 <buihuyhoang9a3@gmail.com>
2021-06-10 15:25:50 +0700

    Done UI

--- packages/web/src/modules/ClassworkAssignmentDetail/ClassworkSubmission/DetailClassworkSubmission.tsx
+++ packages/web/src/modules/ClassworkAssignmentDetail/ClassworkSubmission/DetailClassworkSubmission.tsx
@@ -1,6 +1,21 @@
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 
-import { makeStyles } from '@material-ui/core'
+import { CardContent, Grid, makeStyles } from '@material-ui/core'
+import { useParams } from 'react-router-dom'
+
+import { DASHBOARD_SPACING } from '@kathena/theme'
+import {
+  InfoBlock,
+  PageContainer, SectionCard
+} from '@kathena/ui'
+import { useAuth } from 'common/auth'
+import {
+  useAccountProfileQuery
+} from 'graphql/generated'
+
+
+
+
 
 export type DetailClassworkSubmissionProps = {}
 
@@ -9,7 +24,46 @@ const DetailClassworkSubmission: FC<DetailClassworkSubmissionProps> = (
 ) => {
   const classes = useStyles(props)
 
-  return <div className={classes.root}>DetailClassworkSubmission</div>
+  const params: { id: string } = useParams()
+  const idStudent = useMemo(() => params.id, [params.id])
+  const { account } = useAuth()
+
+  const { data, loading } = useAccountProfileQuery({
+    variables: { username: idStudent },
+  })
+
+  return (
+    <PageContainer
+      withBackButton
+      maxWidth="sm"
+      title="Thông tin bài tập"
+    // subtitle={`@${account?.username}`}
+    // actions={[
+    //   <AccountStatusChip status={account.status} variant="contained" />,
+    // ]}
+    >
+      <Grid container spacing={DASHBOARD_SPACING}>
+        <SectionCard
+          maxContentHeight={false}
+          gridItem={{ xs: 12 }}
+          title="Thông tin tài khoản"
+        >
+
+          <CardContent>
+            <Grid container spacing={2}>
+              <InfoBlock gridItem={{ xs: 12 }} label="Mô tả">
+                Bùi Huy Hoàng
+                {account?.id}
+              </InfoBlock>
+              <InfoBlock gridItem={{ xs: 12 }} label="Bài tập nộp">
+                File
+              </InfoBlock>
+            </Grid>
+          </CardContent>
+        </SectionCard>
+      </Grid>
+    </PageContainer>
+  )
 }
 
 const useStyles = makeStyles(() => ({
