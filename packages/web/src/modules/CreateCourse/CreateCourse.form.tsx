795e7249f1a071d64168147a380018db34cd99bc
nhatminh0509 <leminhnhat1133@gmail.com>
2021-09-19 02:44:05 +0700

    Done

--- packages/web/src/modules/CreateCourse/CreateCourse.form.tsx
+++ packages/web/src/modules/CreateCourse/CreateCourse.form.tsx
@@ -1,18 +1,31 @@
-import { FC, useMemo } from 'react'
+import { FC, useMemo, useState } from 'react'
 
-import { CardContent, makeStyles, Stack } from '@material-ui/core'
+import { CardContent, Checkbox, makeStyles, Stack } from '@material-ui/core'
 import AccountAssignerFormField from 'components/AccountAssigner/AccountAssignerFormField'
 import { useFormikContext } from 'formik'
 
+import { ANY } from '@kathena/types'
 import { CurrencyFormField, SelectFormField, TextFormField } from '@kathena/ui'
-import { useListOrgOfficesQuery } from 'graphql/generated'
+import InputFieldLabel from '@kathena/ui/InputField/InputFieldLabel'
+import { DayOfWeek, useListOrgOfficesQuery } from 'graphql/generated'
 
 import { CourseFormInput, courseLabels as labels } from './CreateCourse'
 
 export type CreateCourseFormProps = {}
 
+const dayOfTheWeekData = [
+  DayOfWeek.Monday,
+  DayOfWeek.Tuesday,
+  DayOfWeek.Wednesday,
+  DayOfWeek.Thursday,
+  DayOfWeek.Friday,
+  DayOfWeek.Saturday,
+  DayOfWeek.Sunday,
+]
+
 const CreateCourseForm: FC<CreateCourseFormProps> = (props) => {
   const classes = useStyles(props)
+  const [daysOfTheWeek, setDaysOfTheWeek] = useState<ANY[]>([])
   const { data } = useListOrgOfficesQuery()
 
   const optionOrgOffices: { label: string; value: string }[] = useMemo(() => {
@@ -28,6 +41,70 @@ const CreateCourseForm: FC<CreateCourseFormProps> = (props) => {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const formik = useFormikContext<CourseFormInput>()
+
+  const onChangeDay = (day: DayOfWeek) => {
+    const arr = [...daysOfTheWeek]
+    const index = arr.findIndex((i) => i.dayOfWeek === day)
+    if (index > -1) {
+      arr.splice(index, 1)
+    } else {
+      arr.push({
+        dayOfWeek: day,
+        startTime: '00:00',
+        endTime: '23:59',
+      })
+    }
+    setDaysOfTheWeek(arr)
+    formik.setFieldValue('daysOfTheWeek', arr)
+  }
+
+  const onChangeStartTime = (day: DayOfWeek, value: string) => {
+    const arr = [...daysOfTheWeek]
+    const index = arr.findIndex((i) => i.dayOfWeek === day)
+    if (index > -1) {
+      const hStart = value.split(':')[0]
+      const mStart = value.split(':')[1]
+      const hEnd = arr[index].endTime.split(':')[0]
+      const mEnd = arr[index].endTime.split(':')[1]
+      if (
+        Number(hStart) < Number(hEnd) ||
+        (Number(hStart) === Number(hEnd) && Number(mStart) <= Number(mEnd))
+      ) {
+        arr[index].startTime = value
+        setDaysOfTheWeek(arr)
+        formik.setFieldValue('daysOfTheWeek', arr)
+      } else {
+        arr[index].startTime = value
+        arr[index].endTime = value
+        setDaysOfTheWeek(arr)
+        formik.setFieldValue('daysOfTheWeek', arr)
+      }
+    }
+  }
+
+  const onChangeEndTime = (day: DayOfWeek, value: string) => {
+    const arr = [...daysOfTheWeek]
+    const index = arr.findIndex((i) => i.dayOfWeek === day)
+    if (index > -1) {
+      const hEnd = value.split(':')[0]
+      const mEnd = value.split(':')[1]
+      const hStart = arr[index].startTime.split(':')[0]
+      const mStart = arr[index].startTime.split(':')[1]
+      if (
+        Number(hEnd) < Number(hStart) ||
+        (Number(hEnd) === Number(hStart) && Number(mEnd) < Number(mStart))
+      ) {
+        arr[index].startTime = value
+        arr[index].endTime = value
+        setDaysOfTheWeek(arr)
+        formik.setFieldValue('daysOfTheWeek', arr)
+      } else {
+        arr[index].endTime = value
+        setDaysOfTheWeek(arr)
+        formik.setFieldValue('daysOfTheWeek', arr)
+      }
+    }
+  }
 
   return (
     <CardContent className={classes.root}>
@@ -60,6 +137,49 @@ const CreateCourseForm: FC<CreateCourseFormProps> = (props) => {
           name="startDate"
           label={labels.startDate}
         />
+        <CurrencyFormField
+          required
+          name="totalNumberOfLessons"
+          label={labels.totalNumberOfLessons}
+        />
+        <div className={classes.inputDayofWeekContainer}>
+          <InputFieldLabel required>{labels.daysOfTheWeek}</InputFieldLabel>
+          <div className={classes.inputDayofWeekWrapper}>
+            {dayOfTheWeekData.map((item) => {
+              const arr = [...daysOfTheWeek]
+              const index = arr.findIndex((i) => i.dayOfWeek === item)
+              const active = index > -1
+              return (
+                <div className={classes.dayOfWeek}>
+                  <Checkbox
+                    checked={active}
+                    onChange={() => onChangeDay(item)}
+                  />
+                  {item}:{' '}
+                  <input
+                    style={
+                      active ? { margin: '0 10px' } : { marginLeft: '10px' }
+                    }
+                    disabled={!active}
+                    value={active ? daysOfTheWeek[index].startTime : '00:00'}
+                    onChange={(e) => onChangeStartTime(item, e.target.value)}
+                    className={classes.input}
+                    type="time"
+                  />{' '}
+                  {`->`}{' '}
+                  <input
+                    style={active ? { margin: '0 10px' } : { marginLeft: 20 }}
+                    disabled={!active}
+                    value={active ? daysOfTheWeek[index].endTime : '23:59'}
+                    className={classes.input}
+                    onChange={(e) => onChangeEndTime(item, e.target.value)}
+                    type="time"
+                  />
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </Stack>
     </CardContent>
   )
@@ -67,6 +187,29 @@ const CreateCourseForm: FC<CreateCourseFormProps> = (props) => {
 
 const useStyles = makeStyles(() => ({
   root: {},
+  inputDayofWeekContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  inputDayofWeekWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  dayOfWeek: {
+    padding: '10px 20px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  input: {
+    borderRadius: '6px',
+    border: 'none',
+    background: '#fff',
+    '&:focus': {
+      borderRadius: '6px',
+      border: 'none',
+      background: '#fff',
+    },
+  },
 }))
 
 export default CreateCourseForm
