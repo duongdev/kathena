54656932c89875e4ce04d6556a9795248750a54c
nhatminh0509 <leminhnhat1133@gmail.com>
2021-10-10 17:14:32 +0700

    In progress

---
+++ packages/web/src/hooks/useNotification/useNotification.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+/* eslint-disable react/jsx-props-no-spreading */
+const useNotifition = () => {
+
+  useEffect(() => {
+
+  }, [])
+}
+
+export default useNotifition
