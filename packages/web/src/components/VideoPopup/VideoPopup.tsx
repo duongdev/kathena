21440ab9685b429fd52751d18790690aca4ebfa9
@buihuyhoang9a3 <buihuyhoang9a3@gmail.com>
2021-12-14 12:01:47 +0700

    Tăng-popup-video*1.5

--- packages/web/src/components/VideoPopup/VideoPopup.tsx
+++ packages/web/src/components/VideoPopup/VideoPopup.tsx
@@ -22,8 +22,9 @@ const VideoPopup: FC<VideoPopupProps> = (props) => {
     <Dialog
       open={open}
       onClose={onClose}
+      right="3.4em"
       extraDialogActions={
-        <>
+        <div style={{ justifyContent: 'space-between' }}>
           <Button
             disabled={index === 0}
             onClick={() => setIndex((state) => state - 1)}
@@ -36,11 +37,19 @@ const VideoPopup: FC<VideoPopupProps> = (props) => {
           >
             Next
           </Button>
-        </>
+        </div>
       }
     >
-      <Typography variant='h3' style={{ marginBottom: 10 }} >{videos[index].title}</Typography>
-      <div style={{ position: 'relative' }}>
+      <div
+        style={{
+          position: 'relative',
+          padding: '0 2em 0 2em',
+        }}
+      >
+        <Typography variant="h3" style={{ marginBottom: 10 }}>
+          {videos[index].title}
+        </Typography>
+
         <div
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{ __html: videos[index].iframe as ANY }}
