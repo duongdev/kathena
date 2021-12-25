0d75dd9ec29d1a2e3ff7e96d8e1f374fe13a1065
nhatminh0509 <leminhnhat1133@gmail.com>
2021-12-25 08:55:54 +0700

    Features

--- packages/web/src/modules/StudyingCourse/Components/ClassworkLessons/ClassworkLessons.tsx
+++ packages/web/src/modules/StudyingCourse/Components/ClassworkLessons/ClassworkLessons.tsx
@@ -49,7 +49,7 @@ const ClassworkLessons: FC<ClassworkLessonsProps> = () => {
         },
       },
     })
-  const classworkLessons = useMemo(
+  const classworkLessonsProps = useMemo(
     () => dataClasswork?.lessons.lessons ?? [],
     [dataClasswork?.lessons.lessons],
   )
@@ -58,6 +58,30 @@ const ClassworkLessons: FC<ClassworkLessonsProps> = () => {
     () => dataClasswork?.lessons.count ?? 0,
     [dataClasswork?.lessons.count],
   )
+
+  const classworkLessons: any[] = useMemo(() => {
+    return classworkLessonsProps.map((item: any, index: any) => {
+      const current = new Date()
+      const startTime = new Date(item.startTime)
+      const beforeLessonStartTime = classworkLessonsProps[index - 1]
+        ? new Date(classworkLessonsProps[index - 1]?.startTime)
+        : current
+      if (
+        startTime.getTime() > current.getTime() &&
+        beforeLessonStartTime.getTime() <= current.getTime()
+      ) {
+        return {
+          ...item,
+          isNext: true,
+        }
+      }
+      return {
+        ...item,
+        isNext: false,
+      }
+    })
+  }, [classworkLessonsProps])
+
   if (!courseId) {
     return <div>Course not found</div>
   }
@@ -84,7 +108,13 @@ const ClassworkLessons: FC<ClassworkLessonsProps> = () => {
                   {
                     label: 'Tiêu đề',
                     render: (classworkLesson, index) => (
-                      <Typography variant="body1" fontWeight="bold">
+                      <Typography
+                        className={
+                          classworkLesson.isNext ? 'title-hightlight' : ''
+                        }
+                        variant="body1"
+                        fontWeight="bold"
+                      >
                         <Link
                           to={buildPath(
                             STUDYING_COURSE_DETAIL_CLASSWORK_LESSON,
@@ -94,7 +124,9 @@ const ClassworkLessons: FC<ClassworkLessonsProps> = () => {
                             },
                           )}
                         >
-                          Buổi {page === 0 ? index + 1 : index + 1 + (page * perPage)}: {classworkLesson.description}
+                          Buổi{' '}
+                          {page === 0 ? index + 1 : index + 1 + page * perPage}:{' '}
+                          {classworkLesson.description}
                         </Link>
                       </Typography>
                     ),
